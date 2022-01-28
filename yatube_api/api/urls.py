c5eee713e219a8d2dbd0f28a0ebe697ff99c33cc
Bazilit <91909975+Bazilit@users.noreply.github.com>
2022-01-28 13:49:54 +0300

    jwt токен
    Добавлены поинты на Follow и jwt токены

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,18 +1,21 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import CommentViewSet, GroupViewSet, PostViewSet
+from api.views import CommentViewSet, GroupViewSet, PostViewSet, FollowViewSet
 
 router = DefaultRouter()
-router.register('posts', PostViewSet, basename='posts')
-router.register('groups', GroupViewSet, basename='groups')
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
 router.register(
     r'^posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
+router.register(r'follow', FollowViewSet, 'followers')
 
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
