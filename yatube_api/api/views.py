d80aa1aca88f73cefbde26ae81e1667e1c3b57f8
Bazilit <91909975+Bazilit@users.noreply.github.com>
2022-01-29 15:09:16 +0300

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -68,7 +68,6 @@ class FollowViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-    
+
     def get_queryset(self):
       return self.request.user.follower.all()
-    
